dc46ef9f01eff96fcfa5d1ab16956796b81e00ea
JavierPersonal <128254676+Javier070@users.noreply.github.com>
2024-05-06 16:31:09 +0200

    logica de JwtUtils

---
+++ src/main/java/com/jm/tfg/JWT/JwtUtils.java
@@ -0,0 +1,62 @@
+package com.jm.tfg.JWT;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.function.Function;
+
+@Service
+public class JwtUtils {
+
+    private String secret = "buenosdias";
+
+    public String extraerUsuarioNombre(String token){
+        return extraerReclamo(token,Claims::getSubject);
+    }
+
+    public Date extraerCaducidad(String token){
+        return extraerReclamo(token,Claims::getExpiration);
+    }
+
+    public <T> T extraerReclamo(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = extraerTodosReclamos(token);
+        return  claimsResolver.apply(claims);
+    }
+
+    public Claims extraerTodosReclamos(String token){
+        return Jwts.parser().setSigningKey(secret).parseClaimsJws(token).getBody();
+    }
+
+    private  Boolean tokenCaducado(String token){
+        return  extraerCaducidad(token).before(new Date());
+    }
+
+
+    public String generarToken(String username,String role){
+        Map <String, Object> claims = new HashMap<>();
+        claims.put("role",role);
+        return crearToken(claims,username);
+    }
+
+    private String crearToken (Map<String, Object> claims, String subject){
+        return Jwts.builder()
+                .setClaims(claims)
+                .setSubject(subject)
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 *60 *10))
+                .signWith(SignatureAlgorithm.HS256,secret).compact();
+    }
+
+    public Boolean validaToken(String token, UserDetails userDetails){
+        final String username = extraerUsuarioNombre(token);
+        return (username.equals(userDetails.getUsername()) && tokenCaducado(token));
+    }
+}
+
---
+++ src/main/java/com/jm/tfg/JWT/SecutotyConfig.java
@@ -0,0 +1,9 @@
+package com.jm.tfg.JWT;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
+public class SecutotyConfig {
+}
